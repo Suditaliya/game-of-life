f8aebf6228c1f48ae4b8138ec5b021b048094138
Sudita <sahasudita@gmail.com>
2020-04-27 11:38:54 +0530

    Updates

---
+++ precache-manifest.5536a9abe6bf0486febe4c7a3d08b15b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d7028920a3ee9f99655",
+    "url": "/game-of-life/static/css/main.2fad7116.chunk.css"
+  },
+  {
+    "revision": "2d7028920a3ee9f99655",
+    "url": "/game-of-life/static/js/main.4b7a7506.chunk.js"
+  },
+  {
+    "revision": "b9d05e43578a5ddb80d1",
+    "url": "/game-of-life/static/js/runtime~main.dfaa7c2d.js"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/game-of-life/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "6b8c3d989e6fc9f7d3b5e05ddc085583",
+    "url": "/game-of-life/index.html"
+  }
+];
